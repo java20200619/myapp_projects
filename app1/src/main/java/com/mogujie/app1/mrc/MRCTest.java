145e82fa36b38d78fe7c4059db771909d2ec97a3
coder-ma-new <mrc1326204901@sina.com>
2020-08-21 16:07:13 +0800

    zzzzz

---
+++ app1/src/main/java/com/mogujie/app1/mrc/MRCTest.java
@@ -0,0 +1,8 @@
+package com.mogujie.app1.mrc;
+
+public class MRCTest {
+
+    public static void main(String[] args) {
+        System.out.println("zzxnb");
+    }
+}
--- app1/src/main/java/com/mogujie/app1/mrc/mrcTest.java
+++
@@ -1,9 +0,0 @@
-package com.mogujie.app1.mrc;
-
-public class mrcTest {
-
-   public void fun1()
-   {
-
-   }
-}
